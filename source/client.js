37e6746518b5ae3cd1d31ff42999f14cd3c7ea5e
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-02-10 18:16:14 +0000

    Add client file with all model data

---
+++ source/client.js
@@ -0,0 +1,104 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import Header from "./components/header";
+import Nav from "./components/nav";
+import Portfolio from "./components/portfolio";
+import About from "./components/about";
+import Contact from "./components/contact";
+
+
+console.clear();
+
+$(function(){ 
+  var navMain = $("#bs-example-navbar-collapse-1");
+  navMain.on("click", "a", null, function () {
+    navMain.collapse('hide');
+  });
+});
+
+let navTitle = "Shubhamoy's Den";
+let siteOwner = "Shubhamoy";
+
+let headerModel = {
+  title: "Experiments with ReactJS",
+  subtitle: "React - Components - JSX - ES6"
+};
+
+let portfolioModel = [
+    {pic:"cabin", link:"#portfolioModal1"},
+    {pic:"cake", link:"#portfolioModal2"},
+    {pic:"circus", link:"#portfolioModal3"},
+    {pic:"game", link:"#portfolioModal4"},
+    {pic:"safe", link:"#portfolioModal5"},
+    {pic:"submarine", link:"#portfolioModal6"}
+];
+
+let footerModel = [
+      {title: "Location", content: "New Delhi, India" },
+      {title: "Around the Web", content: [
+        {link: "https://fb.me/shubhamoy", social:"facebook"},
+        {link: "https://plus.google.com/+ShubhamoyChakrabarty", social:"google-plus"},
+        {link: "https://twitter.com", social:"twitter"},
+        {link: "https://linkedin.com/in/shubhamoy", social:"linkedin"}
+      ]},
+      {title: "About Me", 
+       content: "Shubhamoy loves to experiment with new technologies. Presently he's working with ReactJS :)"}
+];
+
+let mv = {
+  header: {view: Header, model: headerModel, label: "Home"},
+  portfolio: {view: Portfolio, model: portfolioModel, label: "Portfolio"},
+  about: {view: About, label: "About"},
+  contact: {view: Contact, label: "Contact"},
+  footer: {model: footerModel}
+}
+
+ReactDOM.render(<App data={mv} 
+                     ntitle={navTitle}
+                     owner={siteOwner} />, document.getElementById('page-top'));
+  
+/*
+let obj = {a: 5, b: 6};
+let {a, b} = obj;
+*/
+/* ReactDOM.render(<div>Hello World from React</div>, document.getElementById('app')); */
+/*
+let smoy = (a, b) => {
+  console.log(`${a} + ${b} = ${a+b}`);
+}
+*/
+/* import {default as Portfolio, PortfolioItem} from "./components/portfolio"; */
+/*
+console.log('Hello from Webpack 4.0');
+
+function adder(...args) {
+  let sum = 0;
+  for(let item of args) {
+    sum += item;
+  }
+  return sum;
+}
+
+function swaad(n1, ...args) {
+  return n1 * adder.apply(this, args);
+}
+
+console.log(swaad(10,2,3,4,5,5));
+*/
+
+/*
+let model = {
+  nav: navModel,
+  header: headerModel,
+  footer: footerModel,
+  portfolio: portfolioModel
+}
+*/
+/*
+let navModel = [
+    {link:"#portfolio", text: "Portfolio"},
+    {link:"#about", text: "About"},
+    {link:"#contact", text: "Contact"},        
+];
+*/
